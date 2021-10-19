96a3a3b524a8821f1ed8576d52a61e693fb82f2a
Diego C.Sano <diego_ceccarelli@hotmail.com>
2021-10-18 23:48:47 -0300

    Change home  server props to static props

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 
@@ -39,7 +39,7 @@ export default function Home({ product }) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1Jm7khK74N27RjKXUZgEi4bm');
 
   const product = {
@@ -54,5 +54,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
